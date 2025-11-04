32f6ab489efe90d5861475fa1e22bdf4894a7ef9
Ирина Коваленко <kiris-83@mail.ru>
2025-11-04 16:53:11 +0300

    Добавлен gitignor  и прекращено отслежвание папки idea

---
+++ src/mask.py
@@ -0,0 +1,27 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Маскирует номер карты, оставляя первые 6 и последние 4 цифры видимыми."""
+    # Преобразуем число в строку
+    card_str = str(card_number)
+    # Проверяем корректность длины номера карты
+    if len(card_str) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
+    # Фотмируем маску
+    masked = (
+        card_str[:4] + " " +   # Первые 4 цифры
+        card_str[4:6] + "**" + " " +     # Следующие 2 цифры и **
+        "****" + " " +     # 4 звездочки
+        card_str[-4:]     # последние 4 цифры
+    )
+    return masked
+
+
+def get_mask_account(account_number: int) -> str:
+    """ Маскирует номер счёта по шаблону **xxxx"""
+    # Преобразуем число в строку
+    account_str = str(account_number)
+    # Проверяем минимальную длину номера счета
+    if len(account_str) < 6:
+        raise ValueError("Номер счета должен содержать минимум 6 цифр")
+    # Формируем маску
+    masked = "**" + account_str[-4:]
+    return masked
