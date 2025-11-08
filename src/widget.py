6eba39e44f2658bbc69845b950c0ac6c848998d0
Ирина Коваленко <kiris-83@mail.ru>
2025-11-09 01:45:18 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,36 +1,39 @@
 def mask_account_card(account_info: str) -> str:
     """Функция обрабатывает информацию карт и банковских счетов"""
-    account_info = "Visa Platinum 7000111190123456"
+
     # Преобразуем число в строку
     card_str = str(account_info)
-
     # Делим входную строку
     parts = card_str.split()
     if len(parts) != 2:
         raise ValueError("Строка должна содержать тип и номер карты")
-        account_type:Any = parts
-        account_number = parts[-1]
+
+    # Для извлечения частей списка
+    account_number = parts[1]
     # Проверяем корректность формата строки
-        if not account_number.isdigit():
-            raise ValueError("Номер счета должен содержать только цифры")
+    if not account_number.isdigit():
+        raise ValueError("Номер счета должен содержать только цифры")
 
      # Длина строки
-            if len(account_number) > 8:
-                raise ValueError("Номер счета должен содержать минимум 8 цифр")
-     # Формируем маску
-                masked_number = account_number[:4] + "**" (len(account_number)-8) + account_number[-4:]
-            else:
-                masked_number = account_number
-        return account_info
-
+    if len(account_number) < 8:
+        raise ValueError("Номер счета должен содержать минимум 8 цифр")
+    # Формируем маску
+    masked = (
+            account_number[:4] + " " +  # Первые 4 цифры
+            account_number[4:6] + "**" + " " +  # Следующие 2 цифры и **
+            "****" + " " +  # 4 звездочки
+            account_number[-4:]   # последние 4 цифры
+    )
+    return masked
 
 def get_date(change_data: str) -> str:
     """ Функция меняющая дату"""
     user_data = " "
-    for i in change_data[:10].split('-')[::1]:
+# Отделяем первые 10 символов
+    for i in change_data[:10].split('-'):
         if i == "-":
-            user_data += "."
+            user_data += "."     # Разделяем точкой
         else:
-            user_data += 1
+            user_data += i     # Прибавляем значение(цифры)
             return user_data
 
