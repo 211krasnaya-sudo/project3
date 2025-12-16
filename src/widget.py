45c9fa115bd6fbdb7d1019d947313ce849449014
Ирина Коваленко <kiris-83@mail.ru>
2025-12-16 07:13:29 +0300

    Добавлены изменения в файлах

--- src/widget.py
+++ src/widget.py
@@ -14,13 +14,14 @@ def mask_account_card(account_info: str) -> str:
     if not account_number.isdigit():
         raise ValueError("Номер счета должен содержать только цифры")
 
-     # Длина строки
+    # Длина строки
     if len(account_number) < 16:
         raise ValueError("Номер счета должен содержать минимум 16 цифр")
     # Формируем маску
-    masked = account_number[:4] + " " + account_number[4:6] + "**" + " " + "****" + " " + account_number[-4:]   # последние 4 цифры
+    # последние 4 цифры
+    masked = account_number[:4] + " " + account_number[4:6] + "**" + " " + "****" + " " + account_number[-4:]
 
-    return " ".join(parts[:-1]) +  " " + masked
+    return " ".join(parts[:-1]) + " " + masked
 
 
 def get_date(change_data: str) -> str:
@@ -34,4 +35,3 @@ def get_date(change_data: str) -> str:
 # Дата в измененном формате
     user_data = f"{change_data[8:10]}.{change_data[5:7]}.{change_data[0:4]}"
     return user_data
-
