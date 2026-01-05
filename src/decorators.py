7cd4802ab14a176866aee6190da19356d12c9925
Ирина Коваленко <kiris-83@mail.ru>
2026-01-05 15:51:09 +0300

    Добавлен новый фаил decorators

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+
+
+
+
+def log(filename=None):
+    """Декоратор для логирования"""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok\nРезультат: {result}\n"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message)
+                else:
+                    print(log_message)
+                return result
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message)
+                else:
+                    print(log_message)
+
+        return wrapper
+
+    return decorator
