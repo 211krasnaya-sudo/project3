eddb238bd5ad6cb250bf121baff8a0247772661e
Ирина Коваленко <kiris-83@mail.ru>
2026-01-29 16:34:35 +0300

    Добавлены новые файлы

---
+++ src/external_api.py
@@ -0,0 +1,18 @@
+import requests
+
+
+
+def convert_to_rub(amount: float, currency: str) -> float:
+    """
+    Конвертирует сумму из указанной валюты в рубли, используя API.
+    """
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+    headers = {"apikey": API_KEY}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return float(data["result"])
+    else:
+        print(f"Ошибка при запросе к API: {response.status_code} - {response.text}")
+        return 0.0  # Или можно поднять исключение, если не удалось сконвертировать
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
+current_dir = Path(__file__).parent.parent.resolve()
+operations_file_json = current_dir / 'data' / 'operations.json'
+API_KEY = os.getenv('API_KEY')
+
+def transactions_total(transaction: dict) -> float:
+    """
+    Принимает на вход транзакцию и возвращает сумму транзакции в рублях.
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API
+    для получения текущего курса валют и конвертации суммы операции в рубли.
+    """
+    amount = float(transaction["operationAmount"]["amount"])
+    currency = transaction["operationAmount"]["currency"]["code"]
+
+    if currency == "RUB":
+        return amount
+    else:
+        return currency
