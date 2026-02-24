8ed413febf306c3a7aa79386eb0fc5c04f6e3f55
Ирина Коваленко <kiris-83@mail.ru>
2026-02-24 23:27:08 +0300

    fixes

--- src/logging_config.py
+++ src/logging_config.py
@@ -1,11 +1,38 @@
 import logging
 
 
-def setup_logging():
-    logger = logging.getLogger('app')
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('logs/application.log', mode='w')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+def setup_logging(LOG_FILE=None, LOG_FILE_UTILS=None):
+    #Создаем базовый логгер
+    logging.basicConfig(
+        level=logging.INFO,    #Уровень логирования
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        filename=LOG_FILE,
+        filemode='w'
+    )
+    logger = logging.getLogger(__name__)
+
+    # Создаем логгеры для модулей masks, utils
+    masks_logger = logging.getLogger('masks.log')
+    masks_logger.setLevel(logging.INFO)
+
+    utils_logger = logging.getLogger('utils.log')
+    utils_logger.setLevel(logging.INFO)
+
+    #Создаем обработчик для записи логов в файл для модулей masks, utils
+    masks_file_handler = logging.FileHandler(LOG_FILE, mode='w', encoding='utf-8')
+
+    utils_file_handler = logging.FileHandler(LOG_FILE_UTILS, mode='w', encoding='utf-8')
+
+    #Создаем форматер для логов для модулей masks, utils
+    masks_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    masks_file_handler.setFormatter(masks_formater)
+
+    utils_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    utils_file_handler.setFormatter(utils_formater)
+
+    #Добавляем обработчик к логгеру для модулей masks, utils
+    masks_logger.addHandler(masks_file_handler)
+
+    utils_logger.addHandler(utils_file_handler)
+
     return logger
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,22 @@
 import json
 import os
+from logging import exception
 from typing import Union
 
 from src.external_api1 import convert_to_rub
+from src.logging_config import setup_logging
 
+logger = setup_logging()
 
-def transactions_tot(file_path: str) -> list[dict]:
+LOG_DIR = 'logs'
+LOS_FILE_UTILS = os.path.join(LOG_DIR, 'utils.log')
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+def transactions_tot(file_path: str, utils_loger=None) -> list[dict]:
     """Загружает транзакции из JSON-файла."""
+    utils_loger.info(f"Загрузка трансакций из файла: {file_path}")
     try:
         if not os.path.exists(file_path):
             return []
@@ -15,33 +25,42 @@ def transactions_tot(file_path: str) -> list[dict]:
             data = json.load(file)
 
             if isinstance(data, list) and all(isinstance(item, dict) for item in data):
+                utils_loger.info(f"Успешно загружено {len(data)} трансакций из файла {file_path}")
                 return data
             else:
+                utils_loger.warning(f"Файл {file_path} содержит данные не в формате списка. Возвращается пустой список")
                 return []
     except json.JSONDecodeError as e:
+        utils_loger.error(f"Ошибка декодирования JSON в файле: {file_path}")
         raise json.JSONDecodeError("Файл JSON не корректен", e.doc, e.pos)
     except ValueError:
+        utils_loger.error(f"Не удалось преобразить число в файле: {file_path}")
         raise ValueError("Не удалось преобразить число")
     except Exception as e:
-        raise Exception(f"Это общее исключение{e}")
+        utils_loger.exception(f"Произошла ошибка при чтении файла: {exception}")
+        raise Exception(f"Это общее исключение {e}")
 
 
-def get_transaction_amount(transaction: dict) -> Union[float, None]:
+def get_transaction_amount(transaction: dict, utils_loger=None) -> Union[float, None]:
     """Функция для получения суммы транзакции в рублях."""
+    utils_loger.info(f"Запуск функции get_transaction_amount для получения суммы транзакции в рублях: {transaction}")
     try:
         amount = transaction["operationAmount"]["amount"]
         currency = transaction["operationAmount"]["currency"]["code"]
 
         if currency == 'RUB':
+            utils_loger.info(f"Валюта равна рублям {transaction}.")
             return float(amount)
         elif currency in ['USD', 'EUR']:
             return convert_to_rub(amount, currency)
         else:
+            utils_loger.warning(f"Неизвестная валюта. Возвращаем сумму в исходной валюте.")
             print("Неизвестная валюта. Возвращаем сумму в исходной валюте.")
             return float(amount)
 
-    except (ValueError, TypeError):
-        raise ValueError("Не удалось преобразить число")
+    except (ValueError, TypeError) as e:
+        utils_loger.exception(f"Не удалось преобразить число {e}.")
+        raise ValueError("Не удалось преобразить число.")
 
 
 transaction_data = {
