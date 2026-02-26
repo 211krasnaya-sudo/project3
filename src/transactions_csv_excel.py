f04a9cca5e07df5cd2ee4720d7db928b5228ca51
Ирина Коваленко <kiris-83@mail.ru>
2026-02-27 01:47:28 +0300

    Добавлен новый модуль

--- src/transactions_csv_excel.py
+++ src/transactions_csv_excel.py
@@ -1,19 +1,21 @@
-import csv
+from pathlib import Path
+from typing import Union, List, Dict, Hashable, Any
+
 import pandas as pd
-import os
 
 
 
 file_csv = "transactions.csv"
 excel_file = "transactions.excel.xlsx"
 
-def reading_transaction_csv(file_csv: str) -> list[dict]:
+def reading_transaction_csv(file_csv: Union[str, Path]) -> List[Dict[Hashable, Any]]:
     """Функция считывает финансовые операции из CSV-файла."""
     try:
-        reader = pd.read_csv(file_csv, sep=";")
-        return reader.to_dict(orient="records")
+        with open(file_csv, encoding="utf-8") as file:
+            reader = pd.read_csv(file_csv, sep=";")
+            return reader.to_dict(orient="records")
     except FileNotFoundError:
-        print(f"Ошибка: Файл {file_csv} не найден.")
+        print(f"Ошибка: Файл {file_csv} не найден!")
         return []
     except Exception as e:
         print(f"Ошибка при чтении CSV-файла {e}")
@@ -23,8 +25,9 @@ def reading_transaction_csv(file_csv: str) -> list[dict]:
 def reading_transaction_excel(excel_file: str) -> list[dict]:
     """Функция считывает финансовые операции из EXCEL-файла."""
     try:
-        reader = pd.read_excel(excel_file)
-        return reader.to_dict(orient="records")
+        with open(file_csv, encoding="utf-8") as file:
+            reader = pd.read_excel(excel_file)
+            return reader.to_dict(orient="records")
     except FileNotFoundError:
         print(f"Ошибка: Файл {file_csv} не найден.")
     except Exception as e:
