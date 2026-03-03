7e01f6c3787c0628aaf245afffc7fb54d6e8cf85
Ирина Коваленко <kiris-83@mail.ru>
2026-03-04 01:39:11 +0300

    fixes

--- src/transactions_csv_excel.py
+++ src/transactions_csv_excel.py
@@ -1,14 +1,15 @@
+import csv
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, List
 
 import pandas as pd
 
 
-def reading_transaction_csv(file_path: str) -> List[Dict]:
+def reading_transaction_csv(file_path: str, delimiter: str = ";") -> List[dict]:
+
     """Функция считывает финансовые операции из CSV-файла."""
     try:
-        # with open(file_csv, encoding="utf-8") as file:
-        reader = pd.read_csv(file_path)
+        reader = pd.read_csv(file_path, delimiter=delimiter)
         return reader.to_dict(orient="records")
     except FileNotFoundError:
         print(f"Ошибка: Файл {file_path} не найден!")
